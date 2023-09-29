569aec8bb7f0e69a959aaa95dea2b4132983ad13
eddableheath <edable-heath@turing.ac.uk>
2023-09-29 14:15:49 +0100

    :spaghetti: added src with simple noughts and crosses

--- src/simple_alpha_zero/meta_noughts_and_crosses/small_board_game.py
+++ src/simple_alpha_zero/meta_noughts_and_crosses/small_board_game.py
@@ -2,8 +2,6 @@
     Classes and functions for a small (standard) board game of noughts and crosses.
 """
 
-from warnings import warn
-
 import numpy as np
 
 
@@ -28,6 +26,28 @@ class SmallBoard:
         """Get the available positions to play"""
         return np.where(self.state == " ")
 
+    def check_if_won(self):
+        """Check if a player has won, return which player has won."""
+        win_cons = np.array(
+            [
+                [0, 1, 2],
+                [3, 4, 5],
+                [6, 7, 8],
+                [0, 3, 6],
+                [1, 4, 7],
+                [2, 5, 8],
+                [0, 4, 8],
+                [2, 4, 6],
+            ]
+        )
+        for win_con in win_cons:
+            if np.all(self.state[win_con] == "X"):
+                return 0
+            elif np.all(self.state[win_con] == "O"):
+                return 1
+
+        return None
+
     def update_state(self, position: int, player: int):
         """Update the state of the board based on the position
 
@@ -36,7 +56,11 @@ class SmallBoard:
             player: which player is making a move, int=[0,1]
         """
         if self.state[position] != " ":
-            warn("Position already taken! Try Again!")
-            return 1
+            print("Position already taken! Try again!")
+            return
 
         self.state[position] = self.player_states[player]
+
+        player_win = self.check_if_won()
+        if player_win is not None:
+            self.state = np.full(9, self.player_states[player_win])
